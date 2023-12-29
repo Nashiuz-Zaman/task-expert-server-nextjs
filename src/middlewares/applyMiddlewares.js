a05d8c9efdac162da167c2145fa273b933c2ca4c
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2023-12-30 03:48:48 +0600

    Add cookie parser to the applyMiddleware file

--- src/middlewares/applyMiddlewares.js
+++ src/middlewares/applyMiddlewares.js
@@ -1,15 +1,17 @@
 // import necessary packages
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 const applyMiddlewares = (app) => {
   app.use(
     cors({
       origin: true,
-      // credentials: true,
+      credentials: true,
     })
   );
   app.use(express.json());
+  app.use(cookieParser());
 };
 
 // export
