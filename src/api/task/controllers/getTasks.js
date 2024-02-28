b8005f3cfee7a77cdaa9fd5199329ed02790754b
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-02-28 18:03:45 +0600

    Refactor code for tasks controllers

--- src/api/task/controllers/getTasks.js
+++ src/api/task/controllers/getTasks.js
@@ -11,24 +11,15 @@ const getTasks = async (req, res) => {
       }
 
       const filter = { email: email };
-      // const taskInfo = req.body;
-
-      // Change this part when polishing the project in the future
-      // if all tasks are wanted, send all tasks sorted by lastUpdated
-      // if (taskInfo.status === "all") {
-      //   const tasks = await TaskModel.find().sort(sortOption);
-      //   return res.send({ success: true, data: tasks });
-      // }
 
       // set the sorting order
       const sortOption = { lastUpdated: 1 };
       const tasks = await TaskModel.find(filter).sort(sortOption);
-      return res.send({ status: 'success', data: tasks });
+      return res.send({ status: 'success', tasks });
    } catch (error) {
-      console.error('Error fetching tasks:', error);
       return res
          .status(500)
-         .send({ status: 'error', error: 'Internal Server Error' });
+         .send({ status: 'error', customErrMessage: error.message });
    }
 };
 
