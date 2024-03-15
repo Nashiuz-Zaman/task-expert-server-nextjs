29e56427866f82311d531ee12a7b6b664ea6c997
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-03-15 16:30:39 +0600

    Add website route protection route and controller

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -6,6 +6,7 @@ import emailLogin from '../../api/auth/controllers/emailLogin.js';
 import googleLogin from '../../api/auth/controllers/googleLogin.js';
 import initialAuthValidation from '../../api/auth/controllers/initialAuthValidation.js';
 import logout from '../../api/auth/controllers/logout.js';
+import protectRoute from '../../api/auth/controllers/protectRoute.js';
 
 // middlewares
 import verifyToken from './../../middlewares/verifyToken.js';
@@ -18,5 +19,6 @@ authRouter.get('/validate', verifyToken, initialAuthValidation);
 authRouter.post('/email-login', emailLogin);
 authRouter.post('/google-login', googleLogin);
 authRouter.patch('/logout', logout);
+authRouter.get('/route', protectRoute);
 
 export default authRouter;
